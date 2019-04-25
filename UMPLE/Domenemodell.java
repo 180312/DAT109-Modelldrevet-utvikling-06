0fcc74bdc5bd5dc22d442065f3f9ec8924e35bf3
Andreas Garvik <andreas.garvik13@gmail.com>
2019-04-25 11:10:47 +0200

    Update Domenemodell.java

--- UMPLE/Domenemodell.java
+++ UMPLE/Domenemodell.java
@@ -3,13 +3,12 @@ class Professor {
 }
 
 class Jury {
-  1--1..* Expoteam expoteam;
+  1--1..* Project project;
   1--1..* Professor professors;
 }
 
 class Expoteam {
-  1--4 Project project;
-  1--1..* Student students;
+  1--1..4 Student students;
 }
 
 class Student {
@@ -17,6 +16,7 @@ class Student {
 }
 
 class Project  {
+  1--1 Expoteam expoteam;
   1..*--* Spectator spectarors;
 }
 
